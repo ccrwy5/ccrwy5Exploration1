d7b1e485b4e1f3e904533e62ec9af4bd5c582d19
Chris Rehagen <ccrwy5@mail.missouri.edu>
2019-09-26 13:50:21 -0500

    added pytestsAssignment

---
+++ Documents/Senior_Year/CS4320/pytestsAssignment/test_1.py
@@ -0,0 +1,9 @@
+def test1():
+
+	testNumber = 10
+	assert 5 + 5 == testNumber
+
+def test2():
+	
+	testNumber = 100000
+	assert testNumber > 1000
---
+++ Documents/Senior_Year/CS4320/pytestsAssignment/test_2.py
@@ -0,0 +1,8 @@
+import multiply
+
+def test2():
+
+	assert multiply(3,4) == 12
+
+	
+	 
